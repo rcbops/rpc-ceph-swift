6dfdb499ea7c2aaee0e4b9f58966f33337bda5b3
Carlos 'Legionarius' Martinez <carlos.martinez@rackspace.com>
2018-05-03 14:01:57 -0500

    Remove old CC license
    * removed the old CC licenses
    * removed some commented out imports

--- snappy/os_client.py
+++ snappy/os_client.py
@@ -1,18 +1,3 @@
-"""
-Copyright 2013 Rackspace
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-"""
 import hmac
 import requests
 import tarfile
@@ -38,14 +23,14 @@ class ObjectStorageAPIClient(object):
     def __init__(self, storage_url, auth_token, base_container_name=None,
                  base_object_name=None):
         super(ObjectStorageAPIClient, self).__init__()
-        
+
         # This should be passed in instead of being instantiated on its own
         #self.engine_config = EngineConfig()
         #self.temp_dir = expanduser(self.engine_config.temp_directory)
-        
+
         # Temp hack
         self.temp_dir = expanduser('~/.temp')
-        
+
         self.swift_endpoint = storage_url.split('/v1/')[0]
         self.storage_url = storage_url
         self.auth_token = auth_token
@@ -104,7 +89,7 @@ class ObjectStorageAPIClient(object):
 
         # Make the request
         return requests.request(method, url, **requestslib_kwargs)
-    
+
     def put(self, url, **kwargs):
         """ HTTP PUT request """
         return self.request('PUT', url, **kwargs)
